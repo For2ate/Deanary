4f218b38a63ff4f877994a56615ab394ff7a9cce
Ivan <ivanskrypinski@gmail.com>
2024-04-08 20:48:09 +0300

    Добавление базы для проекта, тоесть правильная архитиктура(расположение папок и т.д)

---
+++ DeanarySoft/BuisnessLayer/Sourse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeanarySoft.BuisnessLayer {
+
+    public class Sourse {
+
+        [STAThread()]
+        public static void Main() {
+            App mainApplication = new App();
+            MainWindow mainWindow = new MainWindow();
+            mainApplication.Run(mainWindow);
+        }
+
+
+
+    }
+}
