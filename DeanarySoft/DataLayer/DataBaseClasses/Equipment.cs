4b8d2f984279a6475e70545899dbd99ff2e2f44f
Ivan <ivanskrypinski@gmail.com>
2024-04-15 21:22:56 +0300

    Добавления пары фич и тест конекта к бд

---
+++ DeanarySoft/DataLayer/DataBaseClasses/Equipment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeanarySoft.DataLayer.DataBaseClasses;
+
+public partial class Equipment : IToStringValue
+{
+    public int EquipmentId { get; set; }
+
+    public int ModelId { get; set; }
+
+    public DateOnly DeadlinePeriod { get; set; }
+
+    public DateOnly CommissioningDate { get; set; }
+
+    public string? Description { get; set; }
+
+    public virtual Model Model { get; set; } = null!;
+
+    public override string ToString() {
+        return $"";
+    }
+
+}
---
+++ DeanarySoft/DataLayer/IToStringValue.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeanarySoft.DataLayer {
+    public interface IToStringValue {
+        string ToString();
+    }
+}
